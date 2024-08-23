4ba9bdf711637e347e6c3657e3dd1794aa5b26ee
syuhei176 <syuhei176@gmail.com>
2024-08-23 13:24:50 +0900

    clean

--- src/pages/TransferView.tsx
+++ src/pages/TransferView.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { useBalance, usePrex } from '@prex0/prex-react'
 import {
   PrimaryButton,
